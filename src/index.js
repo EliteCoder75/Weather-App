7de6629761dea9a70ee6c58c6b40ff74302ed347
EliteCoder75 <e.gueddouda.inf@lagh-univ.dz>
2025-03-05 13:43:34 +0100

    setting up basic search input. for a given location in the input we log it's returned object in the console after fetching the wether visual crossing api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.css'
+
+const searchBtn = document.querySelector(".searchBtn");
+const location = document.getElementById("location_id");
+
+console.log(searchBtn);
+async function getData(url) {
+    const response = await fetch(url, {mode: 'cors'});
+    const jsonData = await response.json();
+    console.log(jsonData);
+}
+console.log(location.value);
+
+searchBtn.addEventListener("click", e => {
+    let url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+ location.value +"?key=7WZWAP7QXVP9KJ446MVHALN93";
+    console.log(url);
+    getData(url);
+})
+
