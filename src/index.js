6ca42cb113a5697cc4390e83fbdd47080330df66
EliteCoder75 <e.gueddouda.inf@lagh-univ.dz>
2025-03-05 14:36:16 +0100

    setup basic layout for display and retrieve of some object values from the api response

--- src/index.js
+++ src/index.js
@@ -6,14 +6,17 @@ const location = document.getElementById("location_id");
 console.log(searchBtn);
 async function getData(url) {
     const response = await fetch(url, {mode: 'cors'});
-    const jsonData = await response.json();
-    console.log(jsonData);
+    if (response.status == 200) {
+        let jsonData = await response.json(); 
+        return jsonData;
+      }
+    
+    throw new Error(response.status);
 }
-console.log(location.value);
 
 searchBtn.addEventListener("click", e => {
     let url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+ location.value +"?key=7WZWAP7QXVP9KJ446MVHALN93";
-    console.log(url);
-    getData(url);
-})
-
+    let obj = getData(url);
+    obj.then(result => console.log(result.resolvedAddress, result.currentConditions.conditions, result.currentConditions.datetime,result.currentConditions.temp, result.currentConditions.icon))
+    .catch(err => console.log(err));
+});
